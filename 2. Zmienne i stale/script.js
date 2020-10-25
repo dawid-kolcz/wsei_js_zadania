018a137e7a51c8c9dc5ff93cf086fe65fc9f605d
dawid-kolcz <48647382+dawid-kolcz@users.noreply.github.com>
2020-10-25 11:30:38 +0100

    Ex. 1 and 2 done.

---
+++ 2. Zmienne i stale/script.js
@@ -0,0 +1,26 @@
+//1
+let integer = 1;
+let stringOfChars = "string";
+let booleanValue = true
+let something = undefined
+let nullable = null
+
+console.log(integer);
+console.log(stringOfChars);
+console.log(booleanValue);
+console.log(something);
+console.log(nullable);
+
+//2
+let x = 2;
+let y = 3;
+let sum = 0;
+
+sum = x + y;
+
+console.log(sum);
+
+//3
+let empty;
+console.log(empty);
+//nie zainicjalizowane zmienne przybieraja wartosc undefined, czyli nie zdefinowane
---
+++ 3. Operatory/script.js
@@ -0,0 +1,12 @@
+//1
+let a = true
+let b = false
+console.log(a === b);
+
+//2
+let a = 3;
+let b = 2;
+let moduloResult = a % b;
+console.log(moduloResult);
+
+//3
